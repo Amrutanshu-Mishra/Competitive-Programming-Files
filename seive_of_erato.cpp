c12f74360f29d9784fb8040bfcd7cd4776d0a74e
Amrutanshu <amrutanshumishra607@gmail.com>
2025-05-18 20:13:05 +0530

    feat : implement the seive of eratosthenes

---
+++ seive_of_erato.cpp
@@ -0,0 +1,103 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define mp make_pair
+#define fi first
+#define se second
+#define ll long long
+#define ull unsigned long long
+#define ui unsigned int
+#define us unsigned short
+#define loop(j,n) for(int i = j; i < n;i++)
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int,int> ii;
+typedef vector<ll> vll;
+
+const int M=1e9+7;
+int get_bit(ll n,ll pos){
+    return (n&(1LL<<pos))!=0;
+}
+ll set_bit(ll n,int pos){
+    n=n|(1<<pos);
+    return n;
+}
+ll clear_bit(ll n,int pos){
+    int ele=1<<pos;
+    ele=~ele;
+    n=n&ele;
+    return n;
+}
+void update_bit(int &n,int pos,int value){
+    int mark=~(1<<pos);
+    n=n&mark;
+    n=n|(value<<pos);
+}
+ll gcd(ll a,ll b){
+    if(a==0){
+        return b;
+    }
+    if(b==0){
+        return a;
+    }
+    return gcd(b,a%b);
+}
+ll lcm(ll a,ll b){
+    ll ele=a*b;
+    ll ele1=gcd(a,b);
+    return ele/ele1;
+}
+ll binexp(ll a,ll b,ll m){
+    ll result=1;
+    while(b>0){
+        if(b&1){
+            result=(result*1LL*a)%m;
+        }
+        a=(a*1LL*a)%m;
+        b>>=1;
+    }
+    return result;
+}
+ll modexp(ll a){
+    return binexp(a,M-2,M);
+}
+void solve(){
+   int n;
+   cin>>n;
+   vll arr(n);
+   loop(0,n){
+    cin>>arr[i];
+   }
+}  
+int main(){ 
+    int t;
+    cin>>t;
+    for(int j=0;j<t;j++)
+    {
+        solve();
+    }
+    return 0;
+}
+class Solution {
+public:
+    
+    int countPrimes(int n) {
+        if(n==0 || n==1){
+            return 0;
+        }
+        vector<int>primes(n+1,1);
+        primes[1]=0;
+        primes[0]=0;
+        for(int i=2;i*i<=n;i++){
+            if(primes[i]){
+                for(int j=i*i;j<=n;j+=i){
+                    primes[j]=0;
+                }
+            }
+        }
+        int count=0;
+        for(int i=0;i<n;i++){
+            count+=primes[i];
+        }
+        return count;
+    }
+};
