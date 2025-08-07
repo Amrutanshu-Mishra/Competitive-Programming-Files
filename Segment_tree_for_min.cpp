bb2e9957567bae9ce2cd81f5552f5245f295e112
Amrutanshu <amrutanshumishra607@gmail.com>
2025-08-08 00:30:18 +0530

    solve virtual contest

--- Segment_tree_for_min.cpp
+++ Segment_tree_for_min.cpp
@@ -1,9 +1,144 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class 
+class segtree{
+    int segSize;
+    vector<long long>nums;
+    vector<int>mins;
+
+    public:
+    segtree(int n){
+        segSize=1;
+        while (segSize<n)
+        {
+            segSize*=2;
+        }
+        nums.assign(2*segSize, 0LL);
+        mins.assign(2*segSize, 1LL);
+    }
+
+    //build
+    void build(vector<long long>&a, int x, int lx, int rx){
+        if(rx-lx==1){
+            if(lx<(int)a.size())
+                nums[x]=a[lx];
+            return;
+        }
+        int m=(lx+rx)/2;
+        build(a, 2*x+1, lx, m);
+        build(a, 2*x+2, m, rx);
+        nums[x]=min(nums[2*x+1],nums[2*x+2]);
+        if(nums[2*x+1]==nums[2*x+2]){
+            mins[x]=mins[2*x+1]+mins[2*x+2];
+        }
+        else if(nums[2*x+1]<nums[2*x+2]){
+            mins[x]=mins[2*x+1];
+        }
+        else{
+            mins[x]=mins[2*x+2];
+        }
+    }
+
+    void build(vector<long long>&a){
+        build(a, 0, 0, segSize);
+    }
+
+    void setVal(int i, int v, int x, int lx, int rx){
+        if(rx-lx==1){
+            nums[x]=v;
+            return;
+        }
+        int m=(lx+rx)/2;
+        if(i<m){
+            setVal(i, v, 2*x+1, lx, m);
+        }
+        else{
+            setVal(i, v, 2*x+2, m, rx);
+        }
+        nums[x]=min(nums[2*x+1],nums[2*x+2]);
+        if(nums[2*x+1]==nums[2*x+2]){
+            mins[x]=mins[2*x+1]+mins[2*x+2];
+        }
+        else if(nums[2*x+1]<nums[2*x+2]){
+            mins[x]=mins[2*x+1];
+        }
+        else{
+            mins[x]=mins[2*x+2];
+        }
+    }
+
+    void setVal(int i, int v){
+        setVal(i,v,0,0,segSize);
+    }
+
+    long long minCalc(int l, int r, int x, int lx, int rx){
+        if(lx>=r || l>=rx){
+            return INT64_MAX;
+        }
+        if(lx >=l && rx<=r){
+            return nums[x];
+        }
+        int m=(lx+rx)/2;
+        long long s1 = minCalc(l, r, 2*x+1, lx, m);
+        long long s2 = minCalc(l, r, 2*x+2, m, rx);
+        return min(s1,s2);
+    }
+
+    long long minCalc(int l, int r){
+        return minCalc(l,r,0,0,segSize);
+    }
+
+    long long minCount(int l,int r, int x, int lx, int rx){
+        if(lx>=r || l>=rx){
+            return 0;
+        }
+        if(lx >=l && rx<=r){
+            return mins[x];
+        }
+        int m=(lx+rx)/2;
+        long long c1 = minCount(l, r, 2*x+1, lx, m);
+        long long s1=minCalc(l, r, 2*x+1, lx, m);
+        long long c2 = minCount(l, r, 2*x+2, m, rx);
+        long long s2 = minCalc(l, r, 2*x+2, m, rx);
+        if(s1==s2){
+            return c1+c2;
+        }
+        else if(s1<s2){
+            return c1;
+        }
+        return c2;
+    }
+
+    long long minCount(int l,int r){
+        return minCount(l, r, 0, 0, segSize);
+    }
+};
 
 int main()
 {
+    std::ios_base::sync_with_stdio(false);
+    long long n,m;
+    cin>>n>>m;
+    vector<long long>a(n);
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    segtree st(n);
+    st.build(a);
+    while (m--)
+    {
+        int op;
+        cin>>op;
+        if(op==1){
+            int i,v;
+            cin>>i>>v;
+            st.setVal(i,v);
+        }
+        else{
+            int l,r;
+            cin>>l>>r;
+            cout<<st.minCalc(l,r)<<" "<<st.minCount(l,r)<<"\n";
+        }
+    }
     
 }
--- cfdaily.cpp
+++ cfdaily.cpp
@@ -1,156 +1,149 @@
-#include<bits/stdc++.h>
-// #include<iostream>
-// #include<iomanip>
+#include <bits/stdc++.h>
 using namespace std;
 #define mp make_pair
 #define fi first
 #define se second
-#define ll long long 
+#define ll long long
 #define ull unsigned long long
 #define ui unsigned int
 #define us unsigned short
-#define loop(j,n) for(int i = j; i < n;i++)
-typedef vector<int> vi; 
-typedef vector<vi> vvi; 
-typedef pair<int,int> ii; 
+#define loop(j, n) for (int i = j; i < n; i++)
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int, int> ii;
 typedef vector<ll> vll;
 
-const int M=1e9+7;
-int get_bit(ll n,ll pos){
-    return (n&(1LL<<pos))!=0;
+const int M = 1e9 + 7;
+int get_bit(ll n, ll pos)
+{
+    return (n & (1LL << pos)) != 0;
 }
-ll set_bit(ll n,int pos){
-    n=n|(1<<pos);
+ll set_bit(ll n, int pos)
+{
+    n = n | (1 << pos);
     return n;
 }
-ll clear_bit(ll n,int pos){
-    int ele=1<<pos;
-    ele=~ele;
-    n=n&ele;
+ll clear_bit(ll n, int pos)
+{
+    int ele = 1 << pos;
+    ele = ~ele;
+    n = n & ele;
     return n;
-
 }
-void update_bit(int &n,int pos,int value){
-    int mark=~(1<<pos);
-    n=n&mark;
-    n=n|(value<<pos);
+void update_bit(int &n, int pos, int value)
+{
+    int mark = ~(1 << pos);
+    n = n & mark;
+    n = n | (value << pos);
 }
-ll gcd(ll a,ll b){
-    if(a==0){
+ll gcd(ll a, ll b)
+{
+    if (a == 0)
+    {
         return b;
     }
-    if(b==0){
+    if (b == 0)
+    {
         return a;
     }
-    return gcd(b,a%b);
+    return gcd(b, a % b);
 }
-ll lcm(ll a,ll b){
-    ll ele=a*b;
-    ll ele1=gcd(a,b);
-    return ele/ele1;
+ll lcm(ll a, ll b)
+{
+    ll ele = a * b;
+    ll ele1 = gcd(a, b);
+    return ele / ele1;
 }
-ll binexp(ll a,ll b,ll m){
-    ll result=1;
-    while(b>0){
-        if(b&1){
-            result=(result*1LL*a)%m;
-
+ll binexp(ll a, ll b, ll m)
+{
+    ll result = 1;
+    while (b > 0)
+    {
+        if (b & 1)
+        {
+            result = (result * 1LL * a) % m;
         }
-        a=(a*1LL*a)%m;
-        b>>=1;
+        a = (a * 1LL * a) % m;
+        b >>= 1;
     }
     return result;
 }
-ll modexp(ll a){
-    return binexp(a,M-2,M);
-}
-ll merge(vector<ll> &arr, int low, int mid, int high) {
-    vector<int> temp; // temporary array
-    int left = low;      // starting index of left half of arr
-    int right = mid + 1;   // starting index of right half of arr
-
-    //Modification 1: cnt variable to count the pairs:
-    ll cnt = 0;
-
-    //storing elements in the temporary array in a sorted manner//
-
-    while (left <= mid && right <= high) {
-        if (arr[left] <= arr[right]) {
-            temp.push_back(arr[left]);
-            left++;
-        }
-        else {
-            temp.push_back(arr[right]);
-            cnt += (mid - left + 1); //Modification 2
-            right++;
-        }
-    }
-
-    // if elements on the left half are still left //
-
-    while (left <= mid) {
-        temp.push_back(arr[left]);
-        left++;
-    }
-
-    //  if elements on the right half are still left //
-    while (right <= high) {
-        temp.push_back(arr[right]);
-        right++;
-    }
-
-    // transfering all elements from temporary to arr //
-    for (int i = low; i <= high; i++) {
-        arr[i] = temp[i - low];
-    }
-
-    return cnt; // Modification 3
-}
-
-ll mergeSort(vector<ll> &arr, int low, int high) {
-    ll cnt = 0;
-    if (low >= high) return cnt;
-    int mid = (low + high) / 2 ;
-    cnt += mergeSort(arr, low, mid);  // left half
-    cnt += mergeSort(arr, mid + 1, high); // right half
-    cnt += merge(arr, low, mid, high);  // merging sorted halves
-    return cnt;
-}
-
-ll count_inversions(vector<ll>&a, int n) {
-
-    // Count the number of pairs:
-    return mergeSort(a, 0, n - 1);
+ll modexp(ll a)
+{
+    return binexp(a, M - 2, M);
 }
 
 
-
-void solve(){
-    int n;
+void solve()
+{
+    ll n;
     cin>>n;
-    vi arr(n);
+
+    vll arr(n);
     loop(0,n){
         cin>>arr[i];
     }
-    int ans=0;
-    loop(0,n){
-        ans+=arr[i];
-        if(arr[i]==0){
-            ans++;
+    string s="";
+    vll ans;
+    int i=0;
+    int j=n-1;
+    bool b=true;
+    while(i<j){
+        if(arr[i]==arr[j]){
+            ans.push_back(arr[i]);
+            s+="L";
+            i++;
+        }
+        else if(arr[i]<arr[j]){
+            if(b){
+                ans.push_back(arr[j]);
+                s+="R";
+                j--;
+                b=!b;
+                continue;
+            }
+            else{
+                ans.push_back(arr[i]);
+                s+="L";
+                i++;
+                b=!b;
+                continue;
+            }
+        }
+        else{
+            if(!b){
+                ans.push_back(arr[j]);
+                s+="R";
+                j--;
+                b=!b;
+                continue;
+            }
+            else{
+                ans.push_back(arr[i]);
+                s+="L";
+                i++;
+                b=!b;
+                continue;
+            }
         }
     }
-    cout<<ans<<endl;
-}  
-
-int main(){ 
-    std::ios_base::sync_with_stdio(false);
-    std::cin.tie(NULL);
+    if(i==j){
+        ans.push_back(arr[i]);
+        s+="L";
+    }
+    // for(auto i:ans){
+    //     cout<<i<<" ";
+    // }
+    // cout<<endl;
+    cout<<s<<endl;
+}
+int main()
+{
     int t;
-    t=1;
-    cin>>t;
-    for(int j=0;j<t;j++)
+    cin >> t;
+    for (int j = 0; j < t; j++)
     {
         solve();
     }
-    
+    return 0;
 }
