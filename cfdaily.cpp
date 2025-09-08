38bfac3e131e0a004614aa5101adceffa3f35688
Amrutanshu <amrutanshumishra607@gmail.com>
2025-09-08 19:10:51 +0530

    upsolve

--- cfdaily.cpp
+++ cfdaily.cpp
@@ -1,32 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-typedef long long ll;
-#define all(x) x.begin(), x.end()
-#define vecin(name, len) vector<int> name(len); for (auto &_ : name) cin >> _;
-#define vecout(v) for (auto _ : v) cout << _ << " "; cout << endl;
-
-
-void solve() {
-    int n; cin >> n;
-    vecin(gears, n);
-    set<int> seen;
-    for (auto gear : gears) {
-        if (seen.count(gear)) {
-            cout << "YES" << endl; return;
-        }
-        seen.insert(gear);
-    }
-    cout << "NO" << endl;
-}
-
 int main() {
-	ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    int tt = 1;
-
-    cin >> tt;
-
-    while (tt--) solve();
-    return 0;
+  int t;
+  cin >> t;
+  while(t--) {
+    int x;
+    cin >> x;
+    int mn = 9;
+    while(x > 0) {
+      mn = min(mn, x % 10);
+      x/=10;
+    }
+    cout << mn << endl;
+  }
 }
